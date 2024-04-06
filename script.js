56532fa7b2d09ea3b8ff97d719192698c7d8ea8c
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-04-05 22:49:50 -0400

    Complete play & pause functionality (including title on hover)

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ let isPlaying = false;
 // Play
 function playSong() {
   isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
   music.play();
 };
 
 // Pause
 function pauseSong() {
   isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
   music.pause();
 };
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => isPlaying ? pauseSong() : playSong());
