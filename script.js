689acca0b134e15a1a7a4ce66dad1479d4fd97cd
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-04-12 18:49:59 -0400

    Calculate and display current time

--- script.js
+++ script.js
@@ -97,19 +97,32 @@ loadSong(songs[songIndex]);
 function updateProgressBar(e) {
   if (isPlaying) {
     const { duration, currentTime } = e.srcElement;
+
     // Update progress bar width
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
+
     // Calculate display for duration (duration is in seconds by default)
     const durationMinutes = Math.floor(duration / 60);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
     }
+
     // Delay switching duration element to avoid NaN appearing on screen
     if (durationSeconds) {
       durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
     }
+
+    // Calculate display for current time
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+
+    // Display current time as song progresses
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
 }
 
